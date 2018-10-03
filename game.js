0f565b3574591341e24a79e02f5a051c6925fadd
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-03 16:34:17 -0600

    got hits to screen

--- game.js
+++ game.js
@@ -1,26 +1,33 @@
 var health = 100;
+var hits = 0;
+let enemyName = "Shun Di";
+
 
 function slap() {
   health -= 1
   //alert(health);
   console.log(health)
+  hits += 1
   update()
 }
 function punch() {
   health -= 5
   //alert(health);
   console.log(health)
+  hits += 1
   update()
 }
 function kick() {
   health -= 10
   //alert(health);
   console.log(health)
+  hits += 1
   update()
 }
 
 
 function update() {
   document.getElementById("health").innerHTML = `${health}`;
+  document.getElementById("hits").innerHTML = `${hits}`;
 }
 update()
