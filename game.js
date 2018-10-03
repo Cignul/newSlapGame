bd0a57a086ee11aabd1fa8aa7b619670d838b592
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-03 16:25:17 -0600

    added punch and kick buttons/functions

--- game.js
+++ game.js
@@ -6,9 +6,21 @@ function slap() {
   console.log(health)
   update()
 }
+function punch() {
+  health -= 5
+  //alert(health);
+  console.log(health)
+  update()
+}
+function kick() {
+  health -= 10
+  //alert(health);
+  console.log(health)
+  update()
+}
+
 
 function update() {
   document.getElementById("health").innerHTML = `${health}`;
 }
 update()
-slap()
